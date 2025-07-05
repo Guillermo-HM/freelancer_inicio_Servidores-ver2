2d454e4bda32561ef4394fa377b0b8c8715f66a9
guillermo <guillermo.hernandez5581@alumnos.udg.mx>
2025-07-04 22:06:23 -0600

    corrigiendo puertos

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public")); // Asegúrate de que tus archivos estén en una carpeta llamada "public"
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
