001fa6c2573a1e500b4d2f99f599dc1d63df8b8d
guillermo <guillermo.hernandez5581@alumnos.udg.mx>
2025-07-04 22:47:59 -0600

    First push

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("public")); // Asegúrate de que tus archivos estén en una carpeta llamada "public"
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
